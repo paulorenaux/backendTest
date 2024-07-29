f1cf0f7c0c910f87decffab08ea9b9f0e5e7ad64
linux-paulo <paulo_renaux@hotmail.com>
2024-07-29 19:26:02 -0300

    fixing city names in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,44 +12,44 @@ pr = State.create({ name: 'Paraná', id: 1 })
 sc = State.create({ name: 'Santa Catarina', id: 2 })
 rs = State.create({ name: 'Rio Grande do Sul', id: 3 })
 
-%i['Curitiba'
-   'Londrina'
-   'Maringá '
-   'Ponta Grossa'
-   'Cascavel'
-   'São José dos Pinhais'
-   'Foz do Iguaçu'
-   'Colombo '
-   'Guarapuava'
+['Curitiba',
+   'Londrina',
+   'Maringá ',
+   'Ponta Grossa',
+   'Cascavel',
+   'São José dos Pinhais',
+   'Foz do Iguaçu',
+   'Colombo',
+   'Guarapuava',
    'Araucária'].each do |city|
   City.create({ name: city, state: pr })
 end
 
-%i[
-  'Joinville'
-  'Florianópolis'
-  'Blumenau'
-  'São José'
-  'Itajaí'
-  'Chapecó '
-  'Palhoça'
-  'Criciúma'
-  'Jaraguá do Sul'
-  'Lages'
+[
+  'Joinville',
+  'Florianópolis',
+  'Blumenau',
+  'São José',
+  'Itajaí',
+  'Chapecó',
+  'Palhoça',
+  'Criciúma',
+  'Jaraguá do Sul',
+  'Lages',
 ].each do |city|
   City.create({ name: city, state: sc })
 end
 
-%i[
-  'Porto Alegre'
-  'Caxias do Sul'
-  'Canoas'
-  'Pelotas'
-  'Santa Maria'
-  'Gravataí'
-  'Novo Hamburgo'
-  'Viamão'
-  'São Leopoldo '
+[
+  'Porto Alegre',
+  'Caxias do Sul',
+  'Canoas',
+  'Pelotas',
+  'Santa Maria',
+  'Gravataí',
+  'Novo Hamburgo',
+  'Viamão',
+  'São Leopoldo',
   'Passo Fundo'
 ].each do |city|
   City.create({ name: city, state: rs })
